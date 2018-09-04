ab8ca9b2e64bebef2c5c23e96e0d20417be5d9fb
J-laughter <1131054117@qq.com>
2018-09-04 15:20:01 +0800

    数据结构

--- algorithm/main.cpp
+++ algorithm/main.cpp
@@ -5,10 +5,12 @@ using namespace std;
 
 int main()
 {
+    cout<<"Hello Algorithm"<<endl;
     int a[10] = {2,4,3,4,5};
     SeqList<int> cList(a,5,5);
 
     LinkList<int> cLinkList(a,5);
 
+    //system("pause");
     return 0;
 }
